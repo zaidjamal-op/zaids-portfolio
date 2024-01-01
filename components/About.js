f1e33f6be83e21804fc8bb69a95e64fdfe4498d3
ZAID JAMAL <74419288+zaidjamal-op@users.noreply.github.com>
2024-01-02 04:11:42 +0530

    Updating to node 20

--- components/About.js
+++ components/About.js
@@ -9,14 +9,14 @@ function About() {
             </p>
             <p className="text-base xl:text-xl text-center text-white/70 leading-relaxed mt-4">
                 Transforming visions into immersive digital experiences with expertise in React, React Native, and full-stack development. Passionate about bridging design with functionality, I thrive on crafting seamless user interfaces while contributing to open source and embracing freelance opportunities.
-            </p
+            </p>
             <ScrollIntoView selector="#skills">
                 <div className="mx-auto p-16 hover: cursor-pointer">
                     <FaChevronDown className="animate-bounce mx-auto text-3xl text-white"/>
                 </div>
             </ScrollIntoView>
         </div>
-    )
+    );
 }
 
 export default About
