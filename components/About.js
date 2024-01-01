38b35a8a0939f3fad8427a8a6d0acc34a6f5fd7e
ZAID JAMAL <74419288+zaidjamal-op@users.noreply.github.com>
2024-01-02 03:45:18 +0530

    Update About.js
    Changing description

--- components/About.js
+++ components/About.js
@@ -8,8 +8,8 @@ function About() {
                 {"Hey !"}
             </p>
             <p className="text-base xl:text-xl text-center text-white/70 leading-relaxed mt-4">
-                I'm a passionate <b className="text-white">Frontend Developer</b> (for React and React Native). I love to convert UI design into the real using React. I also love freelancing and working on open source projects.
-            </p>
+                Transforming visions into immersive digital experiences with expertise in React, React Native, and full-stack development. Passionate about bridging design with functionality, I thrive on crafting seamless user interfaces while contributing to open source and embracing freelance opportunities.
+            </p
             <ScrollIntoView selector="#skills">
                 <div className="mx-auto p-16 hover: cursor-pointer">
                     <FaChevronDown className="animate-bounce mx-auto text-3xl text-white"/>
